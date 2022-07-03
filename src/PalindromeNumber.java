accd4a1fcdfd2dbb31e55e0eaa5c9792b76f106e
ARIKARAN S <91272168+Arikaran-13@users.noreply.github.com>
2022-07-03 23:58:24 +0530

    palindromeNumber try2 leetcode 9

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -2,23 +2,26 @@ import java.math.BigInteger;
 import java.util.stream.IntStream;
 
 public class PalindromeNumber {
-    public static Boolean isPalindrome(int n){
-      String temp=String.valueOf(n);
-      StringBuilder sb = new StringBuilder(temp);
-      StringBuilder ans = sb.reverse();
-      Integer k = Integer.valueOf(String.valueOf(ans));
-      int k1=k;
-        System.out.println(k);
-        if(n==k1){
-            return true;
-        }
-        else{
-            return false;
-        }
+    public static boolean isPalindrome(int n){
+        int rev=0;
+        int temp=n;
+      while(n>0){
+          int rem = n%10;
+          rev=rev*10+rem;;
+          n=n/10;
+
+      }
+      if(rev==temp){
+          return true;
+      }
+      else{
+          return false;
+      }
+
     }
     public static void main(String[] args) {
-      int n  =121;
-       Boolean b = isPalindrome(n);
+      int n  =123;
+       boolean b = isPalindrome(n);
         System.out.println(b);
     }
 }
