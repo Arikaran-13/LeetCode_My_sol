590d5798fc78f0d7f05eeda4f2c9c4ececfc3cde
ARIKARAN S <91272168+Arikaran-13@users.noreply.github.com>
2022-07-06 22:59:03 +0530

    Roman_Integer 13 crt ans

--- src/Romen_Integers.java
+++ src/Romen_Integers.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 public class Romen_Integers {
     public static int romanToInt(String str){
-       char[]ch = str.toCharArray();
+       //char[]ch = str.toCharArray();
        HashMap<Character,Integer> hm = new HashMap<>();
        hm.put('I',1);
        hm.put('V',5);
@@ -11,14 +11,24 @@ public class Romen_Integers {
        hm.put('C',100);
        hm.put('D',500);
        hm.put('M',1000);
-       int ans =0;
-       for(char c : ch){
-          ans+= hm.get(c);
+       int res = hm.get(str.charAt(str.length()-1));
+       for(int i=str.length()-2;i>=0;i--){
+
+           int curr = hm.get(str.charAt(i));
+           int nxt = hm.get(str.charAt(i+1));
+           if(hm.get(str.charAt(i))<hm.get(str.charAt(i+1))){
+               res= res-hm.get(str.charAt(i));
+           }
+           else{
+               res = res+hm.get(str.charAt(i));
+           }
        }
-       return ans;
+       return res;
+
+
     }
     public static void main(String[]args){
-        String str = "LVIII";
+        String str = "MCMXCIV";
         System.out.println( romanToInt(str));
     }
 }
