83a3906fd1016f014db3cef9114c23271234094f
ARIKARAN S <91272168+Arikaran-13@users.noreply.github.com>
2022-07-10 20:40:58 +0530

    longesdt string prefix 14

--- src/Longest_Prefix_String.java
+++ src/Longest_Prefix_String.java
@@ -3,29 +3,23 @@ import java.util.Comparator;
 
 public class Longest_Prefix_String {
     public static String longestPrefix(String[]arr){
-        ArrayList<String>al = new ArrayList<>();
 
-        for(int i=1;i<arr.length;i++){
-            String temp1 = arr[i-1];
-            String temp2 = arr[i];
-            int len = Math.min(temp1.length(),temp2.length());
-            String prefix="";
-            for(int j=0;j<len;j++){
-                if(temp1.charAt(j)==temp2.charAt(j)){
-                    prefix += temp1.charAt(j);
+        if(arr.length==0) return "";
+        String prefix  = arr[0];
 
-                }
-                else{
-                    al.add(" ");
-                    al.add(prefix);
-                    break;
-                }
-            }
-        }
-        return al.stream().max(Comparator.comparingInt(String::length)).get();
+      for(int i=1;i<arr.length;i++){
+          while(arr[i].indexOf(prefix)!=0){
+              prefix = prefix.substring(0,prefix.length()-1);
+          }
+
+      }
+      return prefix;
+
+
     }
     public static void main(String[]args){
-        String[]arr = {"dog","racecar","car"};
+        String[]arr = {"flower","flow","flag"};
+        System.out.println();
         System.out.println(longestPrefix(arr));
     }
 }
