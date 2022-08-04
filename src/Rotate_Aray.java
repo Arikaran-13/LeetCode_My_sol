811f644bfbb0f3643d64425bc2a3e10ce78b9b90
ARIKARAN S <91272168+Arikaran-13@users.noreply.github.com>
2022-08-04 08:29:04 +0530

    Leetcode 189

---
+++ src/Rotate_Aray.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+ class Rotate_Aray{
+	public static void rotate(int []arr,int k){
+		int temp=0;
+		while(k-->0){
+
+			temp=arr[0];
+			for(int i=0;i<arr.length-1;i++){
+
+				arr[i]=arr[i+1];
+
+			}
+			arr[arr.length-1]=temp;
+
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+	public static void main (String[] args){
+
+		int [] arr = {10,20,30,40,50,60};
+		int k=3;
+
+		rotate(arr,k);
+	}
+}
